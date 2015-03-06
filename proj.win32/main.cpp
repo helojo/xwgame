365209b11c96b74c86531cb51dd6babf17c37fee
ytfvel <7634103@163.com>
2015-03-06 12:44:02 +0800

    mo

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -17,6 +17,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
     eglView->setViewName("wxgame");
-    eglView->setFrameSize(480, 320);
+    eglView->setFrameSize(1136, 640);
     return CCApplication::sharedApplication()->run();
 }
